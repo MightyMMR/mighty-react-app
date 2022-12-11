5b312258ae6378a1a929b7983d43de7e31fd862b
MightyMMR <minrenmah@gmail.com>
2022-12-12 00:26:00 +0800

    Phase 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 
 const getDefaultCards = () => {
   const list = YUGIOHCards.data
@@ -20,7 +21,7 @@ const getDefaultCards = () => {
       return (
         { ID: card.id, name: card.name, imgID: card.card_images.sort((a, b) => a.id < b.id ? -1 : 1)[0].id, card_detail: card }
       )
-    }).sort((a, b) => a.ID < b.ID ? -1 : 1);
+    }).sort((a, b) => a.name < b.name ? -1 : 1);
   return [...list];
 }
 
@@ -34,7 +35,7 @@ const getDefaultCardsPacks = () => {
 const buildCardPack = (packID, packName, imageID) => {
   const card = defaultCards.find(card => card.card_detail.card_sets != null && card.card_detail.card_sets.find(card_set => card_set.set_name == packName));
   const cardPack = card.card_detail.card_sets.find(card_set => card_set.set_name == packName);
-  return { ID: packID, cardPack, packImg: imageID };
+  return { ID: packID, packName: cardPack.set_name, packImg: imageID, cardPack };
 }
 
 const defaultCardsPacks = [...getDefaultCardsPacks()];
@@ -49,8 +50,27 @@ function App() {
     return [...list];
   }
   const constructCardPack = (param) => {
-    const list = cardPacks.filter(cardPack => cardPack.ID == param);
-    return [...list];
+    const cardSet = {
+      "set_name": param.packName,
+      "set_code": param.packName,
+      "set_rarity": "Ultra Rare",
+      "set_rarity_code": "(C)",
+      "set_price": "999.00"
+    }
+    const newPack = {
+      ID: param.ID,
+      packName: param.packName,
+      packImg: param.cardList[param.cardList.length - 1],
+      cardPack: cardSet
+    };
+    const newCardList = [...cardList];
+    const cardIDList = [...param.cardList];
+    cardIDList.forEach((cardID, index) => {
+      let newCard = newCardList.find(card => card.ID == cardID);
+      newCard.card_detail.card_sets = [...newCard.card_detail.card_sets, cardSet];
+    });
+    setCardPacks([...cardPacks, newPack]);
+    setCardList(newCardList);
   }
 
   return (
@@ -79,8 +99,9 @@ function App() {
                 <Router.Route path='/' element={<HomePage />} />
                 <Router.Route path='/cardpacks' element={<CardPacks cardPacks={cardPacks} />} />
                 <Router.Route path='/cardlist/:packID' element={<CardList cardList={cardList} getCardPack={getCardPack} />} />
-                <Router.Route path='/cardlist/addpack/:packID' element={<CardSelection cardList={cardList} constructCardPack={constructCardPack} />} />
+                <Router.Route path='/cardlist/addpack' element={<CardSelection cardList={cardList} packID={cardPacks.length + 1} constructCardPack={constructCardPack} />} />
                 <Router.Route path='/cardlist' element={<CardList cardList={cardList} cardPack={null} />} />
+                <Router.Route path='*' element={<NotFound />} />
               </Router.Routes>
             </div>
           </div>
@@ -88,6 +109,16 @@ function App() {
       </React.Fragment>
     </Router.BrowserRouter>
   );
+}
+
+function NotFound() {
+  return (
+    <div className='sub-div'>
+      <div className='main-row'>
+        NOT FOUND
+      </div>
+    </div>
+  )
 }
 
 function HomePage() {
@@ -101,12 +132,23 @@ function HomePage() {
 }
 
 function CardPacks(props) {
-  const cardPacks = props.cardPacks;
+  const [searchFilter, setSearchFilter] = React.useState('');
+  const cardPacksList = props.cardPacks;
   const cardBack = { packImg: 'CardBack', cardPack: { set_name: 'Add New Packs' } }
+  const cardPacks = searchFilter != '' ? cardPacksList.filter(cardPack => cardPack.packName.toLowerCase().indexOf(searchFilter.toLowerCase()) >= 0) : cardPacksList;
+  const searchFilterOnChanged = (event) => {
+    const filter = event.target.value;
+    setSearchFilter(filter);
+  }
   return (
     <div className='sub-div'>
       <div className='main-row'>
-        <p>Total No of Packs Available: {cardPacks.length}</p>
+        <div className='no-deco'>
+          <p>Total No of Packs Available: {cardPacks.length}</p>
+        </div>
+        <div className='no-deco m-b'>
+          <input className='input-c' onChange={searchFilterOnChanged} value={searchFilter} placeholder='Search' />
+        </div>
         <div className='sub-div-row'>
           {
             cardPacks.map((cardPack, index) => {
@@ -117,7 +159,7 @@ function CardPacks(props) {
               )
             })
           }
-          <Router.Link to={`/cardlist/addpack/${cardPacks.length + 1}`} className='no-deco'>
+          <Router.Link to='/cardlist/addpack' className='no-deco'>
             <CardPack cardPack={cardBack}></CardPack>
           </Router.Link>
         </div>
@@ -138,12 +180,23 @@ function CardPack(props) {
 
 function CardList(props) {
   let packID = Router.useParams().packID;
+  const [searchFilter, setSearchFilter] = React.useState('');
   const cardPack = packID != null ? props.getCardPack(packID) : null;
-  const cardList = cardPack != null && cardPack.length > 0 ? props.cardList.filter(card => card.card_detail.card_sets.filter(cardset => cardset.set_name == cardPack[0].cardPack.set_name).length > 0) : props.cardList;
+  const cardListFromPack = cardPack != null && cardPack.length > 0 ? props.cardList.filter(card => card.card_detail.card_sets.filter(cardset => cardset.set_name == cardPack[0].cardPack.set_name).length > 0) : props.cardList;
+  const cardList = searchFilter != '' ? cardListFromPack.filter(card => card.name.toLowerCase().indexOf(searchFilter.toLowerCase()) >= 0) : cardListFromPack;
+  const searchFilterOnChanged = (event) => {
+    const filter = event.target.value;
+    setSearchFilter(filter);
+  }
   return (
     <div className='sub-div'>
       <div className='main-row'>
-        <p>Total No of Cards: {cardList.length}</p>
+        <div className='no-deco'>
+          <p>Total No of Cards: {cardList.length}</p>
+        </div>
+        <div className='no-deco m-b'>
+          <input className='input-c' onChange={searchFilterOnChanged} value={searchFilter} placeholder='Search' />
+        </div>
         <div className='sub-div-row'>
           {
             cardList.map((card, index) => {
@@ -169,40 +222,62 @@ function Card(props) {
 }
 
 function CardSelection(props) {
-  let packID = Router.useParams().packID;
-  const [newPackID, setNewPackID] = React.useState(packID);
+  const navigate = Router.useNavigate();
+  const [newPackID, setNewPackID] = React.useState(props.packID);
+  const [newPackName, setNewPackName] = React.useState('');
   const [selectedCardList, setSelectedCardList] = React.useState([]);
-  const cardList = props.cardList;
+  const [searchFilter, setSearchFilter] = React.useState('');
+  const cardList = searchFilter != '' ? props.cardList.filter(card => card.name.toLowerCase().indexOf(searchFilter.toLowerCase()) >= 0) : props.cardList;
   const selectedCard = (cardID, add) => {
     const newList = add ? [...selectedCardList, cardID] : [...selectedCardList.filter(id => id != cardID)];
     setSelectedCardList(newList);
   }
-  const dothis = (cardID, add) => {
-    console.log('click')
+  const nameOnChanged = (event) => {
+    const name = event.target.value;
+    setNewPackName(name);
+  }
+  const submitPack = () => {
+    props.constructCardPack({ ID: newPackID, packName: newPackName, cardList: selectedCardList });
+    setNewPackID(0);
+    setNewPackName('');
+    setSelectedCardList([]);
+    navigate('/cardpacks');
+  }
+  const searchFilterOnChanged = (event) => {
+    const filter = event.target.value;
+    setSearchFilter(filter);
   }
   return (
     <div className='sub-div'>
       <div className='main-row'>
-        <div to='/' className='no-deco'>
-          <Button onClick={dothis}>CONFIRM PACK</Button>
+        <div className='no-deco'>
+          <Button onClick={submitPack}>CONFIRM PACK</Button>
         </div>
-        <p>No of Cards Selected: {selectedCardList.length}</p>
+        <div className='no-deco m-b'>
+          <input className='input-c' onChange={nameOnChanged} value={newPackName} placeholder='Pack Name' />
+        </div>
+        <div className='no-deco m-t'>
+          <p>No of Cards Selected: {selectedCardList.length}</p>
+        </div>
+        <div className='no-deco m-b'>
+          <input className='input-c' onChange={searchFilterOnChanged} value={searchFilter} placeholder='Search' />
+        </div>
         <div className='sub-div-row'>
           {
             cardList.map((card, index) => {
               return (
-                <CardSelect key={index} card={card} selectedCard={selectedCard}></CardSelect>
+                <CardSelect key={index} card={card} selectedCard={selectedCard} selected={selectedCardList.indexOf(card.ID) >= 0}></CardSelect>
               )
             })
           }
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
 function CardSelect(props) {
-  const [active, setActive] = React.useState(false);
+  const [active, setActive] = React.useState(props.selected);
   const card = props.card;
   const selectedCard = (cardID) => {
     props.selectedCard(cardID, !active);
